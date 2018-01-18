13f735ccc2b96c3b203295e5b2abe7b16c12972e
Paul Harter <paul@glowinthedark.co.uk>
2018-01-18 14:35:00 +0000

    a test package

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+
+
 try:
-    from config_local import PI_PASSWORD
+    from config_local import *
 except Exception as e:
     print "ERROR: you have to add a file called config_local.py next to this file with the above values"
--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,33 @@
 from crypt import crypt
 from fabric.api import local, settings, abort, run, env, sudo, put, get, prefix
 import os
+import shutil
 
 BASE_DIR = os.path.dirname(__file__)
-WORKSPACE_DIR = os.path.join(BASE_DIR, "workspace/src")
+WORKSPACE_DIR = os.path.join(BASE_DIR, "workspace")
 
-from config import PI_PASSWORD
+
+# from config import PI_PASSWORD
 
 env.hosts = ["%s:%s" % ("raspberrypi.local", 22)]
 
-env.user = "pi"
-env.password = PI_PASSWORD
+# env.user = "pi"
+# env.password = PI_PASSWORD
 
 
-def init_local():
 
+def catkin_unmake():
+    devel_dir = os.path.join(WORKSPACE_DIR, "devel")
+    build_dir = os.path.join(WORKSPACE_DIR, "build")
+    make_file = os.path.join(WORKSPACE_DIR, "src", "CMakeLists.txt")
+    workspace_file = os.path.join(WORKSPACE_DIR, ".catkin_workspace")
+
+    if os.path.exists(devel_dir):
+        shutil.rmtree(devel_dir)
+    if os.path.exists(build_dir):
+        shutil.rmtree(build_dir)
+    if os.path.exists(make_file):
+        os.remove(make_file)
+    if os.path.exists(workspace_file):
+        os.remove(workspace_file)
 
